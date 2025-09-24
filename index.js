304f6b01da600cbcb9f26786a86dad8d671a93a9
raiizinho <rairodrigues123ofc@gmail.com>
2025-09-24 11:22:06 -0300

    Teste

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 import e from "express";
 import http from "http";
 import { Server } from "socket.io";
+import cors from "cors";
+
 const app = e();
+
+// Configuração CORS para Express
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST"]
+}));
+
 const server = http.createServer(app);
-const io = new Server(server)
+
+// Configuração CORS para Socket.IO
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
 
 io.on("connection", (socket) => {
     console.log("Usuário conectado", socket.id);
@@ -11,11 +27,12 @@ io.on("connection", (socket) => {
     socket.on("disconnect", () => {
         console.log("Usuário desconectado", socket.id);
     });
-})
-app.get("/", (req, res) => {
-  res.json({opa: "amigo"});
 });
 
-app.listen(25508, "0.0.0.0", () => {
-  console.log("rodando na porta http://localhost:25508");
+app.get("/", (req, res) => {
+    res.json({ opa: "amigo" });
+});
+
+server.listen(25508, "0.0.0.0", () => {
+    console.log("rodando na porta http://localhost:25508");
 });
